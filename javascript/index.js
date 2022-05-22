8f0b54095213259c02064d56a5776b20c81ef978
Laambert <98312235+Laambert@users.noreply.github.com>
2022-05-22 11:37:25 +0200

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,18 @@
 const buy = document.getElementById('buy');
 const left = document.getElementById('left');
 const right = document.getElementById('right');
-const galery = document.getElementById("galery");
-let galeryIndex = 0;
+const gallery = document.getElementById("gallery");
+let galleryIndex = 0;
 
-const galeryImages = [
-    " images/australia.jpg",
-    " images/coloseum.webp",
-    " images/garden.jpg"
+const galleryImages = [
+    "images/default.webp", //0
+    "images/australia.jpg", //1
+    "images/coloseum.webp", //2
+    "images/garden.jpg" //3
 ];
 
 function anim() {
-    const Animation = galery.animate([{
+    const Animation = gallery.animate([{
             opacity: "0.5"
         },
         {
@@ -30,27 +31,25 @@ buy.addEventListener('click', function onClick(event) {
 });
 
 left.addEventListener('click', function onClick(event) {
-    if (galeryIndex == 0) {
-        galeryIndex = galeryImages.length;
+    if (galleryIndex == 0) {
+        galleryIndex = galleryImages.length - 1;
         anim();
-        galery.style.backgroundImage = 'url(" images/default.webp")';
+        gallery.style.backgroundImage = "url(" + galleryImages[galleryIndex] + ")";
     } else {
-        galeryIndex -= 1;
         anim();
-        galery.style.backgroundImage = "url(" + galeryImages[galeryIndex] + ")";
-
+        galleryIndex -= 1;
+        gallery.style.backgroundImage = "url(" + galleryImages[galleryIndex] + ")";
     }
 });
 
 right.addEventListener('click', function onClick(event) {
-
-    if (galeryIndex == galeryImages.length) {
-        galeryIndex = 0;
+    if (galleryIndex >= galleryImages.length - 1) {
+        galleryIndex = 0;
         anim();
-        galery.style.backgroundImage = 'url(" images/default.webp")';
+        gallery.style.backgroundImage = "url(" + galleryImages[galleryIndex] + ")";
     } else {
+        galleryIndex += 1;
         anim();
-        galery.style.backgroundImage = "url(" + galeryImages[galeryIndex] + ")";
-        galeryIndex += 1;
+        gallery.style.backgroundImage = "url(" + galleryImages[galleryIndex] + ")";
     }
 });
